0fdda6e51cf21a787bd9cc6bff2fd09ce0183321
Aliyv019 <tahiraliyev2006@gmail.com>
2024-10-06 21:12:57 +0400

    created react app

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './SideBar.module.css'
+
+export default function SideBar() {
+  return (
+    <div className={`bg-white h-full rounded-lg ${styles.sidebar}`}>
+        
+    </div>
+  )
+}
